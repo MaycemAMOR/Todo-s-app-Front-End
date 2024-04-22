56bd665c37ac6366e5f2aab6743d6439450b462b
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 09:14:08 +0200

    - Understanding Bootstrapping of Angular App with Root Module and Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {LoginComponent} from './login/login.component';
-import {FormsModule} from "@angular/forms";
 import {ErrorComponent} from './error/error.component';
 import {ListTodosComponent} from './list-todos/list-todos.component';
 
