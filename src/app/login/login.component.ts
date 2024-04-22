cbd20c7d059792af61b5087949bb340753c060af
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 06:31:44 +0200

    - Understanding Event Binding - Adding click event on Login Page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  username = 'maycem';
+  password = '';
 
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  handleLogin() {
+    console.log(this.username);
+  }
 }
