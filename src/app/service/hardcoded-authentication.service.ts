fec16636665c7400522585e76bece09a1ccc4580
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 10:45:22 +0200

    - Using Session Storage to Store User Authentication Token.

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -9,12 +9,20 @@ export class HardcodedAuthenticationService {
   }
 
   autheticate(username: string, password: string) {
+    console.log('before ' + this.isUserLoggedIn());
     if (username === 'maycem' && password === '06864321') {
+      sessionStorage.setItem('authenticaterUser', username);
+      console.log('after ' + this.isUserLoggedIn());
       return true;
     } else {
       return false;
     }
-
-
   }
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('authenticaterUser');
+    return !(user === null);
+  }
+
+
 }
