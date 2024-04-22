9f8ade1909069e9104068b0a18a767e9fd40bef6
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 07:17:39 +0200

    - Implementing Routes for Login, Welcome and Error Components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {ErrorComponent} from "./error/error.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'login', component:LoginComponent},
+  {path:'welcome', component:WelcomeComponent},
+  {path:'**', component:ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/error/error.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.css'
+})
+export class ErrorComponent {
+  errorMessage = 'An Error Occured! Contact Support at *** - *** - ***';
+
+}
