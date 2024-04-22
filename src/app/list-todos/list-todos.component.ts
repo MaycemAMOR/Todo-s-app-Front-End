a9ad84a47546d1f02c56376ef1d323ed8d7344ed
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 09:02:38 +0200

    - Best Practice - Create a Todo Class. -use  trackBy with *ngFor:  <tr *ngFor="let todo of todos ; trackBy: itemTrackBy"> : C'est tout à notre avantage de permettre au DOM de pouvoir identifier de façon unique chaque élément de nos listes car nous gagnerons nettement en performance en cas de besoin

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 
+
+export class todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date,
+  ) {
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,15 +18,19 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
   todos = [
-    {id: 1, description: 'Learn to Dance'},
-    {id: 2, description: 'Visit India'},
-    {id: 3, description: 'Visit Egypt'},
-    {id: 4, description: 'Visit France'},
-    {id: 5, description: 'Visit USA'},
-    {id: 5, description: 'Visit Canada'}
+    new todo(1, 'Learn to Dance', false, new Date()),
+    new todo(2, 'Visit India', false, new Date()),
+    new todo(3, 'Visit Egypt', false, new Date()),
+    new todo(4, 'Visit France', false, new Date()),
+    new todo(5, 'Visit USA', false, new Date()),
+    new todo(5, 'Visit Canada', false, new Date())
   ]
 
   constructor() {
+  }
+
+  public itemTrackBy(index: number, item: todo) {
+    return item.id;
   }
 
   ngOnInit(): void {
