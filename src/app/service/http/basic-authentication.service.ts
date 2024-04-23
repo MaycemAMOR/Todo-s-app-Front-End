7e6e7c586549e6a18dca99b7945cbc119687df3e
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-24 01:51:51 +0200

    - Refactoring Angular Basic Authentication Service.

--- src/app/service/http/basic-authentication.service.ts
+++ src/app/service/http/basic-authentication.service.ts
@@ -16,33 +16,32 @@ export class BasicAuthenticationService {
   constructor(private http: HttpClient) {
   }
 
-  authenticate(username: string, password: string) {
-    if (username === 'MayTech' && password === '06864321') {
-      sessionStorage.setItem('authenticateUser', username);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  executeHelloWorldBeanService(): Observable<AuthenticationBean> {
-    return this.http.get<AuthenticationBean>('http://localhost:8080/hello-world-bean');
-  }
-
   executeAuthenticationService(username: string, password: string): Observable<AuthenticationBean> {
     /***** on a plus besoin de cette conf grace au service HttpInterceptor ***********/
+   /*** dans ce cas j'ai utilisé ici car j'ai désactivé le httpInterceptor at app.module.ts***/
+
     const basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
     const headers: HttpHeaders = new HttpHeaders({Authorization: basicAuthHeaderString})
     return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`, {headers}).pipe(
       map(
         response => {
           sessionStorage.setItem('authenticateUser', username);
+          sessionStorage.setItem('token', basicAuthHeaderString);
           return response;
         }
       )
     );
   }
 
+  getAuthenticatedUser(): string | null {
+    return sessionStorage.getItem('authenticateUser');
+  }
+
+  getAuthenticatedToken(): string | null {
+    if (this.getAuthenticatedUser()) {
+      return sessionStorage.getItem('token');
+    }
+  }
 
   isUserLoggedIn() {
     const user = sessionStorage.getItem('authenticateUser');
