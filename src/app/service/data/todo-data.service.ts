a1b6cbee7c10e8d66c973ca5f4eba2f334ef0690
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-24 09:27:56 +0200

    - Connecting GET REST APIs to JPA Repository. - Connecting POST, PUT and DELETE REST APIs to JPA Repository.

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -22,23 +22,23 @@ export class TodoDataService {
   }
 
   retrieveAllTodos(name: any): Observable<Todo[]> {
-    return this.http.get<Todo[]>(`${API_URL}/users/${name}/todos`);
+    return this.http.get<Todo[]>(`${API_URL}/jpa/users/${name}/todos`);
   }
 
   deleteTodo(name: any, id: number): Observable<Todo> {
-    return this.http.delete<Todo>(`${API_URL}/users/${name}/todos/${id}`);
+    return this.http.delete<Todo>(`${API_URL}/jpa/users/${name}/todos/${id}`);
   }
 
   getTodo(name: any, id: number | undefined): Observable<Todo> {
-    return this.http.get<Todo>(`${API_URL}/users/${name}/todos/${id}`);
+    return this.http.get<Todo>(`${API_URL}/jpa/users/${name}/todos/${id}`);
   }
 
   updateTodo(name: any, id: number | undefined, todo: Todo) {
-    return this.http.put(`${API_URL}/users/${name}/todos/${id}`, todo);
+    return this.http.put(`${API_URL}/jpa/users/${name}/todos/${id}`, todo);
   }
 
   createTodo(name: any, id: number | undefined, todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(`${API_URL}/users/${name}/todos`, todo);
+    return this.http.post<Todo>(`${API_URL}/jpa/users/${name}/todos`, todo);
   }
 
   public handleError(error: HttpErrorResponse) {
